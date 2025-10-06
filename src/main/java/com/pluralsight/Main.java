2920feb62b364e0c67f7bcaa8a9c9994e560a980
Jaylenh47-stack <jaylenh47@gmail.com>
2025-10-06 13:02:54 -0400

    80% complete

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,115 @@
 package com.pluralsight;
 
+
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Vehicle[] inventory = new Vehicle[20];
+
+    public static void main(String[] args){
+
+        int quantityOfVehicles;
+
+        inventory[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        inventory[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+        inventory[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+        inventory[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+        inventory[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+        inventory[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000);
+        inventory[6]  = new Vehicle(101127, "Nissan Altima", "Gray", 82000, 6800);
+
+        //ToDo loop this menu until the user inputs 6
+        //loop the menu for the user to select from
+        System.out.println("What do you want to do\n");
+        System.out.println("1 - List all vehicles");
+        System.out.println("2 - Search by make/model");
+        System.out.println("3 - Search by price range");
+        System.out.println("4 -Search by color");
+        System.out.println("5 - Add a vehicle");
+        System.out.println("6 - Quit");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                listAllVehicles();
+                break;
+            case 2:
+                makeModelSearch();
+                break;
+            case 3:
+                priceSearch();
+                break;
+            case 4:
+                colorSearch();
+                break;
+            //ToDO case 5 and 6
+        }
+
+
+
+
+
+
+    }
+    //ToDo make these print statements into display() method in ConsoleHelper
+    private static void listAllVehicles(){
+        for (Vehicle i: inventory){
+            if (i == null){
+                continue;
+            }
+            System.out.println(i.getVehicleID() + " " + i.getMakeModel() + " " + "$" + i.getPrice() + " " + i.getColor());
+        }
+    }
+
+    private static void makeModelSearch(){
+        System.out.print("Enter the make and/or model you are searching for");
+        String search = scanner.nextLine();
+
+
+        for (Vehicle i : inventory){
+            if (i == null){
+                continue;
+            }
+            if (i.getMakeModel().toLowerCase().contains(search.toLowerCase())) {
+                System.out.println(i.getVehicleID() + " " + i.getMakeModel() + " " + "$" + i.getPrice() + " " + i.getColor());
+            }
+        }
+    }
+
+    private static void priceSearch(){
+        System.out.print("Enter the minimum price of the vehicles you want to see");
+        int  searchMin = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter the maximum price of the vehicles you want to see");
+        int  searchMax = scanner.nextInt();
+        scanner.nextLine();
+
+        for (Vehicle i : inventory){
+            if (i == null){
+                continue;
+            }
+            if (searchMin <= i .getPrice() && searchMax >= i.getPrice()) {
+                System.out.println(i.getVehicleID() + " " + i.getMakeModel() + " " + "$" + i.getPrice() + " " + i.getColor());
+            }
+        }
+    }
+
+    private static void colorSearch(){
+        System.out.print("Enter the color of the vehicle you are searching for");
+        String search = scanner.nextLine();
+
+        for (Vehicle i : inventory){
+            if (i == null){
+                continue;
+            }
+            if (i.getColor().toLowerCase().contains(search.toLowerCase())) {
+                System.out.println(i.getVehicleID() + " " + i.getMakeModel() + " " + "$" + i.getPrice() + " " + i.getColor());
+            }
+        }
+    }
+
+
+
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,84 @@
 package com.pluralsight;
 
 public class Vehicle {
+   private long vehicleID;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+    public Vehicle(long vehicleID, String makeModel, String color, int odometerReading, float price) {
+        this.makeModel = makeModel;
+        this.vehicleID = vehicleID;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+    }
+
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public long getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(long vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
